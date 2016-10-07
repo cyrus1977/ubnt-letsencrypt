7f363594df3fc82f13c6449518ba91da8d300880
Joongi Kim <me@daybreaker.info>
2016-10-08 00:03:56 +0900

    Update letsrenew.sh
    As of EdgeOS 1.8.5, now we can `set service gui cert-file /config/...` and `set service gui ca-file /config/...` for persistent and officially supported certificate configuration.

--- letsrenew.sh
+++ letsrenew.sh
@@ -11,8 +11,8 @@ python /config/letsencrypt/acme_tiny.py --account-key /config/letsencrypt/accoun
 iptables -D $CHAIN 1
 
 # Copying files to lighttpd directory
-cat /config/letsencrypt/signed.crt > /etc/lighttpd/server.pem
-cat /config/letsencrypt/domain.key >> /etc/lighttpd/server.pem
+cat /config/letsencrypt/signed.crt > /config/letsencrypt/server.pem
+cat /config/letsencrypt/domain.key >> /config/letsencrypt/server.pem
 
 # Restarting lighttpd daemon
 ps -e | grep lighttpd | awk '{print $1;}' | xargs kill
